6f8a2b660d7c773e0dbab9d0820d4455f4f6f9f9
Song San <songsan2012@gmail.com>
2022-05-16 16:24:50 -0700

    Added PlaySound Function from Bip and sound works. Should be complete.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,9 +6,16 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    // -- Create Sound Support
+        var audioPlayer: AVAudioPlayer!
+        
+        let punchSound = "punchSound"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -21,10 +28,27 @@ class ViewController: UIViewController {
         print(sender.tag)
         
         // -- Play the note
-        
-        
+        self.playSound("note\(sender.tag)")
     }
     
+    
+    func playSound(_ name: String) {
+                    
+                    if let sound = NSDataAsset(name: name) {
+                        do {
+                            try audioPlayer = AVAudioPlayer(data: sound.data)
+                            audioPlayer.play()
+                        }
+                        catch
+                        {
+                            print("error")
+                        }
+                    }
+                    else {
+                        print("else")
+                    }
+                    
+    }
     
     
 }
